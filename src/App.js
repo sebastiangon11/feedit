c16937531ef9af12fd95f011724af182a1852f13
Sebas <sebastiangon11@gmail.com>
2021-10-27 08:18:23 -0300

    add form html

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,9 +8,101 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Feed.it</p>
+        <SignUpForm />
+        <LoginForm />
+        <SignOut />
       </header>
     </div>
   );
 }
+
+// eslint-disable-next-line import/first
+import { Auth } from "aws-amplify";
+
+const SignUpForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setpPassword] = useState("");
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { user } = await Auth.signUp({
+        username,
+        password,
+        // attributes: {
+        //     email,
+        //     phone_number,
+        // }
+      });
+      console.log(user);
+    } catch (error) {
+      console.log("error signing up:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setpPassword(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setpPassword] = useState("");
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await Auth.signIn(username, password);
+      console.log("Logged in successfully", user);
+    } catch (error) {
+      console.log("error signing in", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setpPassword(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export const SignOut = () => {
+  const handleLogout = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  };
+  return <button onClick={handleLogout}>Sign out</button>;
+};
 
 export default App;

da9dbe1f07c3e9f437175b81958ded7768187863
Sebas <sebastiangon11@gmail.com>
2021-11-04 08:30:41 -0300

    add profile pic

--- src/pages/auth/components/Logout.js
+++ src/pages/auth/components/Logout.js
@@ -1,7 +1,7 @@
 import { useAuth } from "@contexts/auth";
 
 export const Logout = () => {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
 
   const handleLogout = async () => {
     try {
@@ -10,13 +10,21 @@ export const Logout = () => {
       console.log("error signing out: ", error);
     }
   };
+
   return (
-    <button
-      onClick={handleLogout}
-      className="p-2 active:bg-gray-900 active:top-px active:ring-indigo-900 active:ring-2 underline"
-      type="submit"
-    >
-      Log out
-    </button>
+    <div className="flex items-center	">
+      <img
+        src={user.attributes.picture}
+        className="w-10 h-10 rounded-full"
+        alt={user.username}
+      />
+      <button
+        onClick={handleLogout}
+        className="p-2 active:bg-gray-900 active:top-px active:ring-indigo-900 active:ring-2 underline"
+        type="submit"
+      >
+        Log out
+      </button>
+    </div>
   );
 };
